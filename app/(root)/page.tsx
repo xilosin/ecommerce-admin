e89d41a20f93ccb7e7d6ff6305ea588b3050ab4c
Farhan Khorsheed <ratul097@gmail.com>
2023-07-10 20:25:52 +1000

    [ADDED] Shadcn components and template files

---
+++ app/(root)/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useEffect } from "react"
+
+import { useStoreModal } from "@/hooks/use-store-modal"
+
+const RootPage = () => {
+  const isOpen = useStoreModal((state) => state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen)
+  
+  useEffect(() => {
+    if (!isOpen) { 
+      onOpen() 
+    }
+  }, [isOpen, onOpen])
+  
+
+  return (
+    <div className="p-4">
+      Root Page
+    </div>
+  )
+}
+
+export default RootPage
